4cc30c69b88ef4cdf2f7eee67ab1060fd9301e27
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-07-20 22:50:12 -0700

    Added server

---
+++ client/server.js
@@ -0,0 +1,14 @@
+//dependencies
+const express = require('express'),
+      PORT = process.env.PORT || 3000
+
+
+//initializing the app
+const app = express()
+
+app.use(express.static('client/build'))
+
+
+app.listen(PORT, function(){
+  console.log("Server listening on https://localhost:" + PORT)
+})
