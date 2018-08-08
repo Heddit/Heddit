eb005a443d7d4157457867a834afe0f1b66bbe53
Haley Dandridge <dandridgehc@gmail.com>
2018-08-07 19:50:06 -0700

    working on Reddit

--- src/components/Reddit.js
+++ src/components/Reddit.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
 import RedditData from './RedditLayout'
+import cheerio from 'cheerio'
+import request from 'request'
 
 class Reddit extends Component {
+
+    state = { articles: [] }
+
+    componentDidMount(){
+        this.showReddit();
+      }
+
+
+      showReddit = () =>{
+
+        console.log("scraping reddit")
+        var redditArticles = [];
+    
+        request("https://old.reddit.com/r/webdev/", (error, response, html) => {
+            var $ = cheerio.load(html)
+            
+            $('p.title').each(function () {
+                
+                var title = $(this).title().text();
+
+                // In the currently selected element, look at its child elements (i.e., its a-tags),
+                // then save the values for any "href" attributes that the child elements may have
+                var link = $(this).children().attr("href");
+
+                var newArticle = {
+                    title: title,
+                    link: link,
+                    summary: "We don't have one yet, la ta da tee-da, Loren Ipsum, Kitty Cat Poop Unicorn"
+                }
+    
+                redditArticles.push(newArticle);
+            });
+    
+            this.setState({ articles: redditArticles });
+          });
+        };
+    
+
+
     render() {
+        let {articles} = this.state
         return (
             <div>
-                <RedditData />
+                 { 
+                articles.map(
+                  (article) => {
+                    return <RedditData title={article.title} link={article.link} summary={article.summary}  />
+                  }
+                ) 
+              }
+                
             </div>
         );
     }
